35ba95941c4e97a93d4cad1d111737b9302d6822
Paulinho <paidocanalha1233@gmail.com>
2025-10-03 15:46:54 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    print("Soma 5 + 3 =", soma(5, 3))
+    print("Subtração 5 - 3 =", subtracao(5, 3))
